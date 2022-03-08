b4151ded04446e0f4f95a972f90f4556fff63164
Moisés <moisesnetored@gmail.com>
2022-03-08 16:15:41 -0400

    feat: Home page created, header tests passed, getting data from prismic.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,14 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <div className={styles.header}>
+      <Link href="/">
+        <Image src="/images/logo.svg" alt="logo" width={40} height={40} />
+      </Link>
+      <h1>spacetraveling</h1>
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
-import { GetStaticProps } from 'next';
-
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// import { GetStaticProps } from 'next';
+import { Head } from 'next/document';
+import { FiCalendar } from 'react-icons/fi';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-reactjs';
+import Header from '../components/Header';
 import { getPrismicClient } from '../services/prismic';
-
-import commonStyles from '../styles/common.module.scss';
+// import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
 interface Post {
@@ -24,13 +28,80 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ posts }: any) {
+  return (
+    <>
+      {/* <Head>
+        <title>Home</title>
+      </Head> */}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+      <Header />
 
-//   // TODO
-// };
+      <main className={styles.container}>
+        <div className={styles.boxPostContainer}>
+          {posts.map(post => (
+            <div>
+              <h1>{post.title}</h1>
+              <p>{post.subtitle}</p>
+
+              <div className={styles.detailsPost}>
+                <div>
+                  <FiCalendar fontSize={20} />
+                  <p>{post.updatedAt}</p>
+                </div>
+
+                <div>
+                  <FiCalendar fontSize={20} />
+                  <p>{post.author}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <a href="www">Carregar mais posts</a>
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const postsResponse = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
+    {
+      fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+      pageSize: 1,
+    }
+  );
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      slug: post.uid,
+      // @ts-ignore
+      title: post.data.title,
+      // @ts-ignore
+      subtitle: post.data.subtitle,
+      // @ts-ignore
+      author: post.data.author,
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        'pt-BR',
+        {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        }
+      ),
+    };
+  });
+
+  console.log(JSON.stringify(postsResponse, null, 2));
+
+  // TODO
+  return {
+    props: {
+      posts,
+    },
+  };
+};
